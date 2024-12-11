3ad4047279729f26a850b7492cbf4ee87a43a7d5
sudipta kundu <144679673+Sudipta04@users.noreply.github.com>
2024-12-11 12:10:09 +0530

    Add files via upload

---
+++ calculate average.py
@@ -0,0 +1,13 @@
+
+#write a python program to calculate the average of a list of number
+
+ # numbers  of list 
+numbers = [10, 20, 30, 40, 50]
+# Calculate the sum of the numbers
+total = sum(numbers)
+# len the list
+count = len(numbers)
+# Calculate the average
+average = total / count
+# Print the average
+print("The average is:", average)
